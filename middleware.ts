422471351e242748230ebf86a1c49cf91949c634
tomazcx <tomazcx06@gmail.com>
2022-10-02 14:31:26 -0300

    Added local variable for base url

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,6 @@ export const middleware = (req: NextRequest) => {
 
     if (req.nextUrl.pathname.startsWith('/dashboard'))
         if (req.cookies.get('nextauth.token') === undefined)
-            return NextResponse.redirect("http://localhost:3000")
+            return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL?? "" )
 
 }
