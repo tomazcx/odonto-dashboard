1ae41ea22d7625b18c9857a981d2cb9ae519c7c8
tomazcx <tomazcx06@gmail.com>
2022-10-02 14:40:45 -0300

    Adjusted url

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,6 @@ export const middleware = (req: NextRequest) => {
 
     if (req.nextUrl.pathname.startsWith('/dashboard'))
         if (req.cookies.get('nextauth.token') === undefined)
-            return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL?? "" )
+            return NextResponse.redirect(new URL('/', req.url))
 
 }
